013883368daa62121953e9942e92898a13678b3e
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 12:09:46 +0300

    17-18. Resource Updating Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -41,6 +41,28 @@ app.post('/users', async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  const bodyKeys = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = bodyKeys.every((bodyKey) =>
+    allowedUpdates.includes(bodyKey),
+  );
+
+  if (!isValidOperation)
+    return res.status(400).send({ error: 'Invalid updates' });
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
   try {
@@ -67,6 +89,28 @@ app.post('/tasks', async (req, res) => {
   try {
     await task.save();
     res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const bodyKeys = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = bodyKeys.every((bodyKey) =>
+    allowedUpdates.includes(bodyKey),
+  );
+
+  if (!isValidOperation)
+    return res.status(400).send({ error: 'Invalid updates' });
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // returns updated task
+      runValidators: true,
+    });
+    if (!task) return res.status(404).send();
+    res.send(task);
   } catch (e) {
     res.status(400).send(e);
   }
