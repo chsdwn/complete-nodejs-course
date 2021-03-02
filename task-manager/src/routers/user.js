f511d80afe14877dbffd65b9da28e33a5bd0199b
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 16:38:01 +0300

    7. Serving up Files

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,23 +4,6 @@ const multer = require('multer');
 const User = require('../models/user');
 const router = new express.Router();
 
-router.get('/users/me', auth, async (req, res) => {
-  console.log('user', req.user);
-  res.send(req.user);
-});
-
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) return res.status(400).send();
-    res.send(user);
-  } catch {
-    res.status(500).send();
-  }
-});
-
 router.get('/users', auth, async (_, res) => {
   try {
     const users = await User.find({});
@@ -71,26 +54,6 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
   res.send();
 });
 
-router.patch('/users/me', auth, async (req, res) => {
-  const bodyKeys = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValidOperation = bodyKeys.every((bodyKey) =>
-    allowedUpdates.includes(bodyKey),
-  );
-
-  if (!isValidOperation)
-    return res.status(400).send({ error: 'Invalid updates' });
-
-  try {
-    bodyKeys.forEach((bodyKey) => (req.user[bodyKey] = req.body[bodyKey]));
-    await req.user.save();
-
-    res.send(req.user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(
@@ -127,10 +90,59 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 });
 
+router.get('/users/me', auth, async (req, res) => {
+  console.log('user', req.user);
+  res.send(req.user);
+});
+
 router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove();
     res.send(req.user);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+router.patch('/users/me', auth, async (req, res) => {
+  const bodyKeys = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = bodyKeys.every((bodyKey) =>
+    allowedUpdates.includes(bodyKey),
+  );
+
+  if (!isValidOperation)
+    return res.status(400).send({ error: 'Invalid updates' });
+
+  try {
+    bodyKeys.forEach((bodyKey) => (req.user[bodyKey] = req.body[bodyKey]));
+    await req.user.save();
+
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) throw new Error();
+
+    res.set('Content-Type', 'image/jpg');
+    res.send(user.avatar);
+  } catch {
+    res.status(404).send();
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) return res.status(400).send();
+    res.send(user);
   } catch {
     res.status(500).send();
   }
