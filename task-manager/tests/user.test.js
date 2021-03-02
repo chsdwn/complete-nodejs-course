e55a051b6466e89a76ffb115f9ae352fa8f787ac
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 22:36:45 +0300

    8. Jest Setup and Teardown

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+  name: 'Ali',
+  email: 'ali@mail.com',
+  password: 'Ali12345',
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 it('should signup a new user', async () => {
   await request(app)
@@ -10,4 +22,18 @@ it('should signup a new user', async () => {
       password: 'Hulusi123',
     })
     .expect(201);
+});
+
+it('should login existing user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({ email: userOne.email, password: userOne.password })
+    .expect(200);
+});
+
+it('should not login nonexistent user', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({ email: 'some@mail.com', password: 'SomePassword' })
+    .expect(400);
 });
