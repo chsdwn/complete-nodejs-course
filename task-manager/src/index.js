c4b7503c45c2e57d0790cdc107236e03a22ad0e8
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 11:21:27 +0300

    10-11. Resource Reading Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,12 +9,46 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) return res.status(400).send();
+
+      res.send(user);
+    })
+    .catch(() => res.status(500).send());
+});
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send());
+});
+
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user
     .save()
     .then(() => res.status(201).send(user))
     .catch((error) => res.status(400).send(error));
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) return res.status(400).send();
+
+      res.send(task);
+    })
+    .catch(() => res.status(500).send());
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((tasks) => res.send(tasks))
+    .catch(() => res.status(500).send());
 });
 
 app.post('/tasks', (req, res) => {
