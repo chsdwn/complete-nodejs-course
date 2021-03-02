5e53943304efc1d9f63a8c8be3febf0c49d6eb04
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 13:34:13 +0300

    7. Express Middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,12 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.status(503).send("We're activated maintenance mode. Try again later");
+  // if (req.method === 'GET') res.send('GET requests are disabled');
+  // else next();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
