a7ce8a4a85f6afe27f97bbd3a00a4469063e07cd
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 10:45:42 +0300

    4-5. Data Validation and Sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -8,26 +9,45 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) throw new Error('Email is invalid');
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password'))
+        throw new Error("Password cannot contain 'password'");
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) throw new Error('Age must be a positive number');
+    },
   },
 });
 
 const Task = mongoose.model('Task', {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
-
-const task = new Task({
-  description: 'Learn the Mongoose',
-  completed: false,
-});
-task
-  .save()
-  .then(() => console.log(task))
-  .catch(console.error);
