0add0d0149e0ccb5325069f74799573de6a429df
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 23:39:22 +0300

    14. Testing with Task Data

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest');
 const Task = require('../src/models/task');
 const app = require('../src/app');
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
+const {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree,
+  setupDatabase,
+} = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 
@@ -17,4 +26,25 @@ it('should create task for user', async () => {
   const task = await Task.findById(response.body._id);
   expect(task).not.toBeNull();
   expect(task.completed).toEqual(false);
+});
+
+it('should return tasks for authenticated user', async () => {
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toEqual(2);
+});
+
+it('should not delete other users task', async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
 });
