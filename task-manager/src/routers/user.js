263ed804c4023cd22ee1302a94ade5181f22395c
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 13:09:33 +0300

    4. Logging in Users

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -67,4 +67,16 @@ router.delete('/users/:id', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password,
+    );
+    res.send(user);
+  } catch {
+    res.status(400).send();
+  }
+});
+
 module.exports = router;
