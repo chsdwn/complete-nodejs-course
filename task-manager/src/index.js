e76b3567fd6a26ab140619e33f31721d7b69e5e3
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-02 12:14:24 +0300

    19. Resouce Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -63,6 +63,16 @@ app.patch('/users/:id', async (req, res) => {
   }
 });
 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).send();
+    res.send(user);
+  } catch {
+    res.status(500).send();
+  }
+});
+
 app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
   try {
@@ -113,6 +123,16 @@ app.patch('/tasks/:id', async (req, res) => {
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).send();
+    res.send(task);
+  } catch {
+    res.status(500).send();
   }
 });
 
