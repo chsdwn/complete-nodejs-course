58f0c3a84f7501a7fedd62c1a182bfb3ec66326e
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-03 10:59:46 +0300

    7. Broadcasting Events

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -16,9 +16,14 @@ io.on('connection', (socket) => {
   console.log('New connection');
 
   socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined');
 
   socket.on('sendMessage', (msg) => {
     io.emit('sendMessage', msg);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left');
   });
 });
 
