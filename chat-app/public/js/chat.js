caa8e45c6fe03dd1f53a2666f9db628e56ff8f72
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-03 10:55:38 +0300

    6. Socket.io Events Challenge

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,12 @@
-io();
+const socket = io();
+const sendBtn = document.getElementById('sendBtn');
+const msgInput = document.getElementById('msgInput');
+
+socket.on('message', (msg) => console.log(msg));
+
+socket.on('sendMessage', (msg) => console.log(msg));
+
+sendBtn.addEventListener('click', () => {
+  socket.emit('sendMessage', msgInput.value);
+  msgInput.textContent = '';
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,21 +1,27 @@
-const http = require("http");
-const path = require("path");
-const express = require("express");
-const socketio = require("socket.io");
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, "../public");
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
-  console.log("New connection");
+io.on('connection', (socket) => {
+  console.log('New connection');
+
+  socket.emit('message', 'Welcome!');
+
+  socket.on('sendMessage', (msg) => {
+    io.emit('sendMessage', msg);
+  });
 });
 
 server.listen(port, () => {
-  console.log("Listening ", port);
+  console.log('Listening ', port);
 });
