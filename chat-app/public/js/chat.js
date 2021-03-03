b4b81ac4a126a78160ab1048494983b59dfb5f95
Hulusi Kafalıer <hulusikafalier@gmail.com>
2021-03-03 11:37:10 +0300

    10. Form and Button States

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -7,19 +7,28 @@ socket.on('message', (msg) => console.log(msg));
 socket.on('sendMessage', (msg) => console.log(msg));
 
 sendBtn.addEventListener('click', () => {
+  sendBtn.setAttribute('disabled', 'disabled');
+
   socket.emit('sendMessage', msgInput.value, (msg) => {
+    sendBtn.removeAttribute('disabled');
+    msgInput.value = '';
+    msgInput.focus();
+
     console.log(msg);
   });
-  msgInput.textContent = '';
 });
 
 shareLocationBtn.addEventListener('click', () => {
   if (!navigator.geolocation)
     return alert('Your browser does not supports geolocation');
 
+  shareLocationBtn.setAttribute('disabled', 'disabled');
+
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
     socket.emit('sendLocation', { ltd: latitude, lng: longitude }, (msg) => {
+      shareLocationBtn.removeAttribute('disabled');
+
       console.log(msg);
     });
   });
